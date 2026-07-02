63928ec08c74a8931e564a5682818cb8ef4bd80e
Kairo Cardoso <kairoc91@gmail.com>
2026-07-02 13:37:19 -0300

    change files folder

--- 1-beginner/javascript/1001.js
+++
@@ -1,6 +0,0 @@
-let A = parseInt(lines.shift());
-let B = parseInt(lines.shift());
-
-let X = A + B;
-
-console.log(`X = ${X}`);
--- 1-beginner/javascript/1016.js
+++
@@ -1,7 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-let distance = parseInt(input.trim());
-let time = distance * 2;
-
-console.log(`${time} minutos`);
